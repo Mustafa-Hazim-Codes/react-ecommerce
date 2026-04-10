a8c8cf6affec68d17574e132f173fe275bc66683
Mustafa <hazimmustafa77@gmail.com>
2026-04-10 19:18:38 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
+import Page404 from "./components/Page404";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Shop</h2>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
