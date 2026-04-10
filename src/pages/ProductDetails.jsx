1d151e8986b513ba28f61540a01709859c5692dd
Mustafa <hazimmustafa77@gmail.com>
2026-04-10 22:07:29 +0200

    create product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,26 @@
-function ProductDetails() {
-    return (
-        <div>
-            Product details
-        </div>
-    )
-}
+import { useParams } from "react-router-dom";
+import products from "../data/products";
 
-export default ProductDetails
+const ProductDetails = () => {
+  const { id } = useParams();
+
+  const product = products.find((p) => p.id === Number(id));
+
+  if (!product) {
+    return <h2>Product not found</h2>;
+  }
+
+  return (
+    <div className="product-details">
+      <img src={product.image} alt={product.title} />
+
+      <div className="details">
+        <h2>{product.title}</h2>
+        <p className="price">${product.price}</p>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
