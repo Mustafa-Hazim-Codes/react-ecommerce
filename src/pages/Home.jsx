41586cdea39463926cd3a6595335f2f1ad821735
Mustafa <hazimmustafa77@gmail.com>
2026-04-10 21:50:36 +0200

    add styling edit placeholders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <div>
+      <h1>Welcome to Shop</h1>
+      <p>Browse our products</p>
+    </div>
+  );
 };
 
 export default Home;
