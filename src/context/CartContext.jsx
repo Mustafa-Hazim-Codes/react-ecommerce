673a4ad9843a0b4d00f8c278db400635214fabcd
Mustafa <hazimmustafa77@gmail.com>
2026-04-11 22:59:39 +0200

    persist cart data using localStorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState(() => {
+        const storedCart = localStorage.getItem("cart");
+
+        return storedCart ? JSON.parse(storedCart) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+    }, [cartItems]);
 
     // Add to cart
     const addToCart = (product) => {
