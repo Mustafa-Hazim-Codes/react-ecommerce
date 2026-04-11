b4da8e7fa5e74604444ae5c074e842444fa849db
Mustafa <hazimmustafa77@gmail.com>
2026-04-11 23:46:42 +0200

    display empty cart message

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 
 const Cart = () => {
   const { cartItems, updateQuantity, removeFromCart, getTotalPrice } = useCart();
 
   if (cartItems.length === 0) {
-    return <h2>Your cart is empty</h2>;
+    return (
+      <div className="empty-cart">
+        <h2>Your cart is empty 🛒</h2>
+        <p>Looks like you haven’t added anything yet.</p>
+
+        <Link to="/" className="shop-btn">
+          Go Shopping
+        </Link>
+      </div>
+    );
   }
 
   return (
